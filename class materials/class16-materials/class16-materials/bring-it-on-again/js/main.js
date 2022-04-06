2d06b4c63993369f852a39f07d479d7539f33339
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-06 22:55:37 +0100

    finished the kata

--- class materials/class16-materials/class16-materials/bring-it-on-again/js/main.js
+++ class materials/class16-materials/class16-materials/bring-it-on-again/js/main.js
@@ -10,8 +10,37 @@
 
 // *Conditionals*
 // Create a function that takes in 3 numbers. Starting with 100 add the first number, subtract the second, and divide the third. If the value is greater then 25, console log "WE HAVE A WINNNA"
+// function bringItOn(x, y, z){
+// let a= 100;
+// a += x;
+// a -= y;
+// a /= z;
+// // console.log(a);
+// // if(a < 25){
+// // console.log('we have a winner');
+// // }
+// }
+// bringItOn(20, 22, 10);
+
 
 // Create a function that takes in a day of the week. If it is a weekend alert, "weekend" and if not alert "week day". Handle capitilization and if the user does not enter a day of the week alert "Try again!"
+// let day = prompt('what day is it?').toLocaleLowerCase()
+// if(day = 'monday' || 'tuesday' || 'wednesday' || 'thursday' || 'friday'){
+//  console.log('it\'s work hour');
+// }else if('saturday' || 'sunday'){
+// console.log('it\'s the weekend');
+// }else{
+//  console.log('try again');
+// }
+ 
+ 
+
+
+
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number or greater, but count by 3
+
+// for(let i = 0; i < 10; i+=3){
+//  console.log(i);
+// }
