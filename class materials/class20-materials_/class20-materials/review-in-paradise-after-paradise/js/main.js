295d764062e0a3277aa230dbddb463261341e779
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-07 18:38:09 +0100

    done

--- class materials/class20-materials_/class20-materials/review-in-paradise-after-paradise/js/main.js
+++ class materials/class20-materials_/class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,19 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+let numArr = [100,23,33,56,101];
+
+// console.log(numArr[numArr.length - 1]);
+function compare(){
+numArr.forEach(x=>{
+ if(numArr[0] < numArr[numArr.length - 1]){
+console.log('hi');
+ }else if(numArr[0] > numArr[numArr.length - 1]){
+  console.log('bye');
+ }else if(numArr[0] = numArr[numArr.length - 1]){
+console.log('we close in hour');
+ }
+ 
+})
+
+}
+compare();
