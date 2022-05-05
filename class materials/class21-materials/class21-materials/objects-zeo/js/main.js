2ca41e5b4cdaa36acc55aed94d896e028d29679e
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-05 12:44:09 +0100

    more javascript lessons

--- class materials/class21-materials/class21-materials/objects-zeo/js/main.js
+++ class materials/class21-materials/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,13 @@
 //Create a stopwatch object that has four properties and three methods
+function Stopwatch(color, weight, brand, time) {
+  this.color = color;
+  this.weight = weight;
+  this.brand = brand;
+  this.time = new Date(time);
+}
+Stopwatch.prototype.alarm = function () {
+  return `wake up! it's ${this.time}, your birthday`;
+};
+
+const casio = new Stopwatch('red', '1.3kg', 'casio', '12-02-1997');
+console.log(casio.alarm());
--- class materials/class22-materials/class22-materials/turbo-review/js/main.js
+++ class materials/class22-materials/class22-materials/turbo-review/js/main.js
@@ -1,7 +1,14 @@
 // *Variables*
 // Declare a variable and assign it to your fav drink as a string. Make sure there is no whitespace on either side of the string, and print the value to the console
 
+// let myFavoriteDrink = 'yoghurt'
+// console.log(myFavoriteDrink);
+
 //Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
+
+// let listOfDrinkFlavors = 'pineapple, apple, orange, mango'
+// console.log(listOfDrinkFlavors.charAt(2));
+
 
 // *Functions*
 // Create a function that returns rock, paper, or scissors as randomly as possible
