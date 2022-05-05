ab1f13e277a7a7ebd7d721ad49db64ee6f72af7c
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-05-05 14:40:55 +0100

    good

--- class materials/class21-materials/class21-materials/objects-street-fighter/js/main.js
+++ class materials/class21-materials/class21-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,14 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighter(strength, finishing, endurance, xp) {
+  this.strength = strength;
+  this.finishing = finishing;
+  this.endurance = endurance;
+  this.xp = xp;
+
+  this.skill = function () {
+    return `iron bow tackle + ${(this.xp /= 10)}xp`;
+  };
+}
+
+const liuKang = new StreetFighter('160ibs', 'freeze block', 'moderate', 110);
+console.log(liuKang);
